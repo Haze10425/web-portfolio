214e7900675bc419566572938ace501a9ae8d541
Haze10425 <jheee042500@gmail.com>
2025-06-10 22:27:48 +0900

    chore: gsap 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+gsap.registerPlugin(useGSAP);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
